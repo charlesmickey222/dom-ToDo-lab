8c310ca76feb3b7d29ddb282a42dbe9abc3b93b5
CharlieMickey <charles.f.mickey@gmail.com>
2022-12-08 02:51:15 -0600

    complete event listener for to-do list

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
-const inputElement = document.querySelector('input');
-const submitBtn = document.querySelector('.submit-button');
-const toDoList = document.querySelector('.todo-list');
+const inputElement = document.querySelector('#input-field');
+const submitBtn = document.querySelector('#submit-button');
+const toDoList = document.querySelector('#todo-list');
 
+submitBtn.addEventListener('click', function(){
+  const newThing = document.createElement('li');
+  newThing.innerText = inputElement.value;
+  toDoList.appendChild(newThing);
+  inputElement.value = '';
+},);
+
+document.querySelectorAll('li').addEventListener('click',function(){})
